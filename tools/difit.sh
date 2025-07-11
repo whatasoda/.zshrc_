1edd503e41818b2560fb96ef3f7ce139fbff3910
whatasoda <shota.hatada@whatasoda.me>
2025-07-11 20:38:32 +0900

    fix(gifit): make first choice inclusive

--- tools/difit.sh
+++ tools/difit.sh
@@ -1,13 +1,13 @@
 alias difit="bunx --bun difit"
 
 gifit() {
-  local base_commit target_commit base_hash
+  local from_commit to_commit from_hash to_hash
 
-  base_commit=$(git log --oneline --decorate -100 | fzf --prompt "BASE(2nd arg)> ") || return
-  base_hash="${base_commit%% *}"
+  from_commit=$(git log --oneline --decorate -100 | fzf --prompt "FROM(inclusive)> ") || return
+  from_hash="${from_commit%% *}"
 
-  target_commit=$(git log --oneline --decorate -100 $base_hash~1.. | fzf --prompt "TARGET(1st arg)> ") || return
-  target_hash="${target_commit%% *}"
+  to_commit=$(git log --oneline --decorate -100 $from_hash~1.. | fzf --prompt "TO(inclusive)> ") || return
+  to_hash="${to_commit%% *}"
 
-  difit "$target_hash" "$base_hash"
+  difit "$to_hash" "$from_hash~1"
 }
