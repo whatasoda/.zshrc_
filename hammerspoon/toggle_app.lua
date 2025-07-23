f110bd1934f4015722892f399cd8f30a087d0cee
whatasoda <shota.hatada@whatasoda.me>
2025-07-23 15:19:23 +0900

    chore: update

--- hammerspoon/toggle_app.lua
+++ hammerspoon/toggle_app.lua
@@ -1,15 +1,13 @@
-local apps = {
-  d = "com.todesktop.230313mzl4w4u92",
-  -- d = "com.microsoft.VSCode",
-  s = "com.mitchellh.ghostty",
-  g = "com.tinyspeck.slackmacgap"
-}
+local IDE_APP_ID = "com.todesktop.230313mzl4w4u92"
+local TERMINAL_APP_ID = "com.mitchellh.ghostty"
+local CALENDAR_APP_NAME = "Notion Calendar"
 
-local function toggleAppByBundleID(bundleID)
+local function switchAppByBundleID(bundleID, toggle)
   local app = hs.application.get(bundleID)
-
   if app and app:isFrontmost() then
-    app:hide()
+    if toggle then
+      app:hide()
+    end
   elseif app then
     app:activate()
   else
@@ -17,23 +15,93 @@ local function toggleAppByBundleID(bundleID)
   end
 end
 
+local function hideAppByBundleID(bundleID)
+  local app = hs.application.get(bundleID)
+  if app then
+    app:hide()
+  end
+end
+
+
+local function switchAppByName(appName, toggle)
+  local app = hs.application.find(appName)
+  if app and app:isFrontmost() then
+    if toggle then
+      app:hide()
+    end
+  elseif app then
+    app:activate()
+  else
+    hs.application.launchOrFocus(appName)
+  end
+end
+
+local function showIDE()
+  switchAppByBundleID(IDE_APP_ID, false)
+  hs.eventtap.keyStroke({ "ctrl", "shift", "command" }, "w")
+end
+
+local function toggleIDE()
+  switchAppByBundleID(IDE_APP_ID, true)
+end
+
+local function toggleTerminal()
+  switchAppByBundleID(TERMINAL_APP_ID, true)
+end
+
+local function hideIDEAndTerminal()
+  hideAppByBundleID(IDE_APP_ID)
+  hideAppByBundleID(TERMINAL_APP_ID)
+end
+
+local function toggleCalendar()
+  switchAppByName(CALENDAR_APP_NAME, true)
+end
+
+local function moveTabToLeft()
+  if hs.application.frontmostApplication():bundleID() == IDE_APP_ID then
+    hs.eventtap.keyStroke({ "control", "shift", "command", "option" }, "left")
+  end
+
+  hs.eventtap.keyStroke({ "control", "shift" }, "tab")
+end
+
+local function moveTabToRight()
+  if hs.application.frontmostApplication():bundleID() == IDE_APP_ID then
+    hs.eventtap.keyStroke({ "control", "shift", "command", "option" }, "right")
+  end
+
+  hs.eventtap.keyStroke({ "control" }, "tab")
+end
+
 -- イベントタップ定義（global にして再スタート可能に）
 local tap = hs.eventtap.new({ hs.eventtap.event.types.keyDown }, function(event)
   local flags = event:getFlags()
   local keyCode = event:getKeyCode()
 
-  if flags:containExactly({ "ctrl" }) then
-    for key, bundleID in pairs(apps) do
-      if keyCode == hs.keycodes.map[string.upper(key)] then
-        local ok, err = pcall(function()
-          toggleAppByBundleID(bundleID)
-        end)
-        if not ok then
-          hs.alert.show("toggle error: " .. tostring(err))
-        end
-        return true
-      end
-    end
+  if flags:containExactly({ "ctrl" }) and keyCode == hs.keycodes.map["A"] then
+    hideIDEAndTerminal()
+    return true
+  end
+
+  if flags:containExactly({ "ctrl" }) and keyCode == hs.keycodes.map["S"] then
+    toggleTerminal()
+    return true
+  end
+
+  if flags:containExactly({ "ctrl" }) and keyCode == hs.keycodes.map["D"] then
+    toggleIDE()
+    return true
+  end
+
+  if flags:containExactly({ "ctrl" }) and keyCode == hs.keycodes.map["W"] then
+    showIDE()
+    return true
+  end
+
+  if flags:containExactly({ "ctrl", "shift" }) and keyCode == hs.keycodes.map["Q"] then
+    toggleCalendar()
+    return true
   end
 
   return false
@@ -41,20 +109,32 @@ end)
 
 tap:start()
 
-hs.hotkey.bind({ "ctrl" }, "D", function()
+hs.hotkey.bind({ "ctrl" }, "a", function()
   hs.alert.show("🔄 Reloading Hammerspoon config")
-  toggleAppByBundleID(apps.d)
+  hideIDEAndTerminal()
   hs.reload()
 end)
 
-hs.hotkey.bind({ "ctrl" }, "S", function()
+hs.hotkey.bind({ "ctrl" }, "s", function()
   hs.alert.show("🔄 Reloading Hammerspoon config")
-  toggleAppByBundleID(apps.s)
+  toggleTerminal()
   hs.reload()
 end)
 
-hs.hotkey.bind({ "ctrl" }, "G", function()
+hs.hotkey.bind({ "ctrl" }, "w", function()
   hs.alert.show("🔄 Reloading Hammerspoon config")
-  toggleAppByBundleID(apps.g)
+  showIDE()
+  hs.reload()
+end)
+
+hs.hotkey.bind({ "ctrl" }, "d", function()
+  hs.alert.show("🔄 Reloading Hammerspoon config")
+  toggleIDE()
+  hs.reload()
+end)
+
+hs.hotkey.bind({ "ctrl", "shift" }, "q", function()
+  hs.alert.show("🔄 Reloading Hammerspoon config")
+  toggleCalendar()
   hs.reload()
 end)
